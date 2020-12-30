ef9b1432b2feda072eb018ab4a94bca4fd5e64c6
Sergey <redmoloch@yanadex.ru>
2020-12-30 11:41:41 +0300

    lesson 1

--- src/index.js
+++ src/index.js
@@ -1,13 +1,38 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
 
+const Header = () => <h2>!!!Hello Hell!!!</h2>;
+const Input = () => <input placeholder="input here"></input>;
+const Btn = () => {
+  const press = 'Press Here'
+  return <button>{press} (4+5)</button>;
+};
+
+const App = () => {
+  return ( 
+    <div>
+      <Header/>
+      <Input/>
+      <Btn/>
+    </div>
+  )
+};
+
+const elem = <div>
+  <h2>Hello Hell!</h2>
+  <input placeholder="input here"></input>
+  <button>press here</button>
+  </div>;
+
+
+//const elem = React.createElement('h2', null, '!Hello Hell!')
+
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  // elem,
+  <App/>,
   document.getElementById('root')
 );
 
