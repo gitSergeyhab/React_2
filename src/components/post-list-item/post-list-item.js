93fca71678b168ff81fa1a36f1751c0fc118eaad
Sergey <redmoloch@yanadex.ru>
2021-01-02 18:26:13 +0300

    git fixed styles

--- src/components/post-list-item/post-list-item.js
+++ src/components/post-list-item/post-list-item.js
@@ -1,13 +1,15 @@
 import React from 'react';
 
+import './post-list-item.css';
+
 
 const PostListItem = () => {
     return (
-    <li className='app-list-item d-flex justify-content-between'>
-        <span className='app-list-item-label'>
+    <li className='app-list-item d-flex justify-content-between' >
+        <span className='app-list-item-label qq'>
             Hellooo
         </span>
-        <div>
+        <div className='d-flex justify-content-center align-items-center'>
             <button type='button' className='btn-star btn-sm'>
                 <i className='fa fa-star'></i>
             </button>
