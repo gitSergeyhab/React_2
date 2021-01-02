bf72afb03e176dafce4fe97af114dd213727d6ca
Sergey <redmoloch@yanadex.ru>
2021-01-02 13:59:46 +0300

    styles added

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -1,10 +1,12 @@
 import React from 'react';
 
+import './app-header.css';
+
 const AppHeader = () => {
     return (
         <div className="app-header d-flex">
             <h2>Header</h2>
-            <h3>5 записей, из них понравилось 1</h3>
+            <h3>5 записей, из них понравилось 0</h3>
         </div>
     )
 }
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import './index.css';
 // import App from './App';
 import reportWebVitals from './reportWebVitals';
 import App from './components/app/app';
-// import AppHeader from './components/app-header/'
+
+import './index.css';
 
 ReactDOM.render(
   <App/>,
